c1abc3503a0f0933e9b4fa5e4825d327430587dd
loahpotato <964040805@qq.com>
2022-11-26 23:36:30 +0100

    ink

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import tools
 
 
 def hist(data, save=None, x_label=None):
@@ -16,23 +17,14 @@ def hist(data, save=None, x_label=None):
     for c, p in zip(col, patches):
         plt.setp(p, 'facecolor', cm(c))
     plt.tick_params(labelsize=24)
-    ax.yaxis.offsetText.set_fontsize(24)
+    ax.axis.offsetText.set_fontsize(24)
     ax.set_xlabel(x_label, fontsize=20, fontweight='semibold')
     ax.set_ylabel('Count', fontsize=20, fontweight='semibold')
     fig.show()
     # fig.savefig(save+'.pdf')
 
 
-'''df = pd.read_csv('data/mnist.csv')
-df1 = df.iloc[0]
-print(df1)
-print(df1.describe())'''
-
-mnist_data = pd.read_csv('data/mnist.csv').values
-labels = mnist_data[:, 0]
-digits = mnist_data[:, 1:]
-img_size = 28
-# print(np.max(digits))
+labels, digits = tools.read_data()
 mask = digits != 0
 new_data = digits[mask]
 hist(new_data)
---
+++ ink_visualize.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tools
+
+
+labels, digits = tools.read_data()
+
+# create ink feature
+ink = np.array([sum(row) for row in digits])
+# compute mean for each digit class
+ink_mean = [np.mean(ink[labels == i]) for i in range(10)]
+# compute standard deviation for each digit class
+ink_std = [np.std(ink[labels == i]) for i in range(10)]
+plt.figure(figsize=[10, 10])
+x = np.array(range(10))
+plt.errorbar(x, ink_mean, yerr=ink_std, fmt='o', ecolor='r', color='b', elinewidth=2, capsize=4)
+plt.xlabel('digits', fontsize=20, fontweight='semibold')
+plt.title('Error bars for each digit class', fontsize=24, fontweight='semibold')
+plt.xticks(size=20)
+plt.yticks(size=20)
+plt.show()
+
