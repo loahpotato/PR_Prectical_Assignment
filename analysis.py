387b54ba5e56a85d42a458bd59fa2e4c96a37fdb
Siyi Liu <964040805@qq.com>
2022-11-28 22:57:29 +0100

    images

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import tools
 
 
-def hist(data, save=None, x_label=None):
+def hist(data, save=None, x_label=None, title=None):
     b = [i for i in range(0, 261, 10)]
     hists, bins = np.histogram(data, bins=b)
     fig = plt.figure(figsize=[15, 15])
@@ -17,14 +17,23 @@ def hist(data, save=None, x_label=None):
     for c, p in zip(col, patches):
         plt.setp(p, 'facecolor', cm(c))
     plt.tick_params(labelsize=24)
-    ax.axis.offsetText.set_fontsize(24)
-    ax.set_xlabel(x_label, fontsize=20, fontweight='semibold')
-    ax.set_ylabel('Count', fontsize=20, fontweight='semibold')
+    ax.yaxis.offsetText.set_fontsize(24)
+    ax.set_xlabel(x_label, fontsize=25, fontweight='semibold')
+    ax.set_ylabel('Count', fontsize=25, fontweight='semibold')
+    plt.title(title, fontsize=30, fontweight='semibold')
     fig.show()
-    # fig.savefig(save+'.pdf')
+    fig.savefig('./images/'+save+'.pdf')
 
+
+df = pd.read_csv('data/mnist.csv')
+values = df.values
+print(df.dtypes)
+print(df.shape)
+print(df.label.value_counts())
+print(df.isnull().sum())
+print(df.duplicated().sum())
 
 labels, digits = tools.read_data()
-mask = digits != 0
-new_data = digits[mask]
-hist(new_data)
+no_zero = digits[digits != 0]
+hist(digits, save='zero', x_label='pixel values')
+hist(no_zero, save='no_zero', x_label='pixel values', title='Histogram of pixel values(non-zero)')
--- ink_visualize.py
+++ ink_visualize.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tools
@@ -15,9 +14,9 @@ ink_std = [np.std(ink[labels == i]) for i in range(10)]
 plt.figure(figsize=[10, 10])
 x = np.array(range(10))
 plt.errorbar(x, ink_mean, yerr=ink_std, fmt='o', ecolor='r', color='b', elinewidth=2, capsize=4)
-plt.xlabel('digit classes', fontsize=20, fontweight='semibold')
-plt.title('Error bars of ink feature', fontsize=24, fontweight='semibold')
+plt.xlabel('digit classes', fontsize=25, fontweight='semibold')
+plt.title('Error bars of ink feature', fontsize=30, fontweight='semibold')
 plt.xticks(size=20)
 plt.yticks(size=20)
 plt.show()
-
+plt.savefig('./images/error_bar.pdf')
